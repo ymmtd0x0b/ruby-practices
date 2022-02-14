9627020851ee5671a70a30d63fb0e5ced0608d9b
ymmtd0x0b <ymmtd0x0b@gmail.com>
2022-02-15 07:51:30 +0900

    オプション内のデータをハッシュからインスタンス変数で保持するように変更&エラー処理の削除

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -2,29 +2,27 @@
 
 require 'date'
 require 'optparse'
-require_relative './class'
+require_relative './one_year_calendar'
+require_relative './three_months_calendar'
+require_relative './one_month_calendar'
+require_relative './calendar_option'
 
 option = CalendarOption.new
 
 cal =
-  case option.options.keys.join(',')
-  when 'year'
-    OneYearCalendar.new(option.get_value(:year))
-  when 'month'
-    OneMonthCalendar.new(Date.today.year, option.get_value(:month))
-  when 'year,month', 'month,year'
-    OneMonthCalendar.new(option.get_value(:year), option.get_value(:month))
-  when 'one_year'
-    OneYearCalendar.new(Date.today.year)
-  when 'three_months'
+  if option.off_highlight
+    OneMonthCalendar.new(Date.today.year, Date.today.month).off_highlight
+  elsif option.three_months
     prev_month = Date.today.prev_month
     ThreeMonthsCalendar.new(prev_month)
-  when 'off_highlight'
-    OneMonthCalendar.new(Date.today.year, Date.today.month).off_highlight
-  when ''
-    OneMonthCalendar.new(Date.today.year, Date.today.month)
+  elsif option.year && option.month
+    OneMonthCalendar.new(option.year, option.month)
+  elsif option.month
+    OneMonthCalendar.new(Date.today.year, option.month)
+  elsif option.year || option.one_year
+    OneYearCalendar.new(Date.today.year)
   else
-    exit
+    OneMonthCalendar.new(Date.today.year, Date.today.month)
   end
 
 cal.print
--- 02.calendar/calendar_option.rb
+++ 02.calendar/calendar_option.rb
@@ -1,95 +1,26 @@
 # frozen_string_literal: true
 
 class CalendarOption
-  attr_reader :options
+  attr_reader :year, :month, :off_highlight, :one_year, :three_months
 
   def initialize
-    @options = {}
     OptionParser.new do |opt|
-      opt.on('-y [value]', '--year [value]') { |value| @options[:year] = value }
-      opt.on('-m [value]', '--month [value]') { |value| @options[:month] = value }
-      opt.on('-h') { @options[:off_highlight] = true }
-      opt.on('-1') { @options[:one_year] = true }
-      opt.on('-3') { @options[:three_months] = true }
-      begin
-        opt.parse!(ARGV)
-      rescue StandardError => e
-        print "cal: invalid option -- '#{e.message.gsub(/.+: -/, '')}'\n"
-        exit
+      opt.on('-y [value]', '--year [value]') { |value| @year = value.to_i }
+      opt.on('-m value', '--month value') { |value| @month = value.to_i }
+      opt.on('-h') { @off_highlight = true }
+      opt.on('-1') { @one_year = true }
+      opt.on('-3') { @three_months = true }
+      opt.parse!(ARGV)
+
+      if @year.nil? && @month.nil?
+        case ARGV.size
+        when 2
+          @year = ARGV[1].to_i
+          @month = ARGV[0].to_i
+        when 1
+          @year = ARGV[0].to_i
+        end
       end
-      check_argument
-      check_option_value
-    end
-  end
-
-  def get_value(name)
-    @options[name]
-  end
-
-  private
-
-  def has?(name)
-    @options.include?(name)
-  end
-
-  def check_year_value
-    value = get_value(:year)
-    if value.nil?
-      @options[:year] = Date.today.year
-    elsif value.start_with?(/\A[0-9]+\z/)
-      @options[:year] = value.to_i
-    else # 引数が存在し、数字以外が含まれていればエラー
-      print "cal: not a valid year #{@options[:year]}\n"
-      exit
-    end
-  end
-
-  def check_month_value
-    value = get_value(:month)
-    if value.nil?
-      print "cal: option requires an argument -- 'm'\n"
-      exit
-    end
-
-    num = value.to_i
-    if (1..12).cover?(num)
-      @options[:month] = num
-    else
-      print "cal: #{value} is neither a month number (1..12) nor a name\n"
-      exit
-    end
-  end
-
-  def check_year_and_month_with_value
-    if has?(:month) && get_value(:year).nil? # cal -y year -m month において、片方の引数が与えられていない場合はエラーとして終了
-      print "cal: option requires an argument -- 'y'\n"
-      exit
-    elsif has?(:year) && get_value(:month).nil?
-      print "cal: option requires an argument -- 'm'\n"
-      exit
-    end
-  end
-
-  def check_option_value
-    case @options.keys.join
-    when 'year'
-      check_year_value
-    when 'month'
-      check_month_value
-    when 'yearmonth', 'monthyear'
-      check_year_value
-      check_month_value
-      check_year_and_month_with_value
-    end
-  end
-
-  def check_argument
-    case ARGV.size
-    when 2 # 引数２つで月・年の順で指定されたと見做す
-      @options[:month] = ARGV[0]
-      @options[:year] = ARGV[1]
-    when 1 # 引数１つで年のみ指定されたと見做す
-      @options[:year] = ARGV[0]
     end
   end
 end
