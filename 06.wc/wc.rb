0ff880716a9a844df138489bcae2d633e7788624
ymmtd0x0b <ymmtd0x0b@gmail.com>
2022-03-14 05:53:52 +0900

    標準入力/複数引数時の処理の追加

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -7,16 +7,56 @@
 
 #  - 標準入力：常に文字列(ファイルを開いた状態)として処理する
 
+DEFAULT_DIGIT_IN_STDIN = 7
+DEFAULT_DIGIT_IN_ARGV = 1
+
 def main
-  contents = ARGV.empty? ? STDIN.readlines : File.readlines(ARGV[0])
-  wc(contents)
+  ARGV.empty? ? stdin : argv
 end
 
-def wc(contents)
-  lines = contents.count
-  words = contents.join.split(nil).count
-  size = contents.join.bytesize
-  printf("%2s%3s %s %s\n", lines, words, size, ARGV[0])
+def stdin
+  result = wc(STDIN.readlines, '')
+  digit = DEFAULT_DIGIT_IN_STDIN
+  sum = 0
+  result.each do |key, value|
+    next if key.eql?(:name)
+    digit = value.digits.size if digit < value.digits.size
+  end
+  display(result, digit)
+end
+
+def argv
+  files =
+    ARGV.map do |arg|
+      wc(File.readlines(arg), arg)
+    end
+
+  digit = DEFAULT_DIGIT_IN_ARGV
+  sum = {name: "合計", lines: 0, words: 0, size: 0}
+  files.each do |file|
+    file.each do |key, value|
+      next if key.eql?(:name)
+      sum[key] += value
+      digit = sum[key].digits.size if digit < sum[key].digits.size
+    end
+  end
+
+  files.each { |file| display(file, digit) }
+  printf("%#{digit}s %#{digit}s %#{digit}s %s\n", sum[:lines], sum[:words], sum[:size], sum[:name]) if ARGV.size >= 2
+
+end
+
+def wc(contents, arg)
+  {
+    name: arg,
+    lines: contents.count,
+    words: contents.join.split(nil).count,
+    size: contents.join.bytesize
+  }
+end
+
+def display(file, digit)
+  printf("%#{digit}s %#{digit}s %#{digit}s %s\n", file[:lines], file[:words], file[:size], file[:name])
 end
 
 main
